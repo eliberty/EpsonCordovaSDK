b285bb9352c771a482d5a09179d0b397dc4476e8
kceliberty <kentin.claret@eliberty.fr>
2025-05-06 11:30:36 +0200

    print epson

--- src/android/EpsonPrinterPlugin.java
+++ src/android/EpsonPrinterPlugin.java
@@ -2,21 +2,18 @@ package com.eliberty.cordova.plugin.epsonusb;
 
 import com.epson.epos2.printer.Printer;
 import com.epson.epos2.Epos2Exception;
+import com.epson.epos2.printer.ReceiveListener;
+import com.epson.epos2.printer.PrinterStatusInfo;
 
 import android.content.Context;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.hardware.usb.UsbInterface;
-import android.hardware.usb.UsbEndpoint;
-import android.hardware.usb.UsbDeviceConnection;
+import android.util.Log;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EpsonPrinterPlugin extends CordovaPlugin {
 
@@ -25,26 +22,123 @@ public class EpsonPrinterPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d("EpsonPrinterPlugin", "execute called");
+        Log.d("EpsonPrinterPlugin", "Args: " + args.toString());
         if (action.equals("printText")) {
-            this.printText(callbackContext);
+            String toPrint = args.getJSONObject(0).getString("text");
+            Log.d("EpsonPlugin", "Texte reçu pour impression : " + toPrint);
+            this.printText(callbackContext, toPrint);
+            return true;
+        }
+        if (action.equals("isPrinterAvailable")) {
+            this.isPrinterAvailable(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void printText(CallbackContext callbackContext) {
+    private void printText(CallbackContext callbackContext, String textToPrint) {
         try {
-            Printer printer = new Printer(Printer.TM_T88, Printer.MODEL_ANK, cordova.getActivity());
-            printer.addText("Hello depuis Cordova !\n");
-            printer.addCut(Printer.CUT_FEED);
+            Log.d("EpsonPrinterPlugin", "Avant instanciation Printer");
+            Context context = cordova.getActivity().getApplicationContext();
+            Printer printer = new Printer(Printer.TM_T88, Printer.MODEL_ANK, context);
+            Log.d("EpsonPrinterPlugin", "Printer instancié");
+
             printer.connect("USB:", Printer.PARAM_DEFAULT);
             printer.beginTransaction();
+
+            PrinterStatusInfo status = printer.getStatus();
+
+            boolean isOnline = status != null && status.getConnection() == Printer.TRUE && status.getOnline() == Printer.TRUE;
+
+            if (!isOnline) {
+                callbackContext.error("Imprimante hors ligne");
+                printer.endTransaction();
+                printer.disconnect();
+                return;
+            }
+
+            Pattern qrPattern = Pattern.compile("<QRCODE>(.*?)</QRCODE>", Pattern.DOTALL);
+            Matcher matcher = qrPattern.matcher(textToPrint);
+
+            int currentIndex = 0;
+            while (matcher.find()) {
+                int qrStart = matcher.start();
+                int qrEnd = matcher.end();
+                String textBefore = textToPrint.substring(currentIndex, qrStart);
+
+                // Imprimer le texte AVANT la balise QR
+                if (!textBefore.isEmpty()) {
+                    printer.addTextAlign(Printer.ALIGN_CENTER);
+                    printer.addText(textBefore);
+                }
+
+                // Générer le QRCode à la place de la balise
+                String qrContent = matcher.group(1);
+                if (qrContent != null && !qrContent.isEmpty()) {
+                    printer.addTextAlign(Printer.ALIGN_CENTER);
+                    printer.addSymbol(qrContent, Printer.SYMBOL_QRCODE_MODEL_2,
+                            Printer.LEVEL_L,
+                            8, // size (3 to 16)
+                            1, 0);
+                }
+
+                currentIndex = qrEnd; // Avancer l'index pour continuer après le QR
+            }
+
+            // Texte après le dernier QRCode
+            if (currentIndex < textToPrint.length()) {
+                String textAfter = textToPrint.substring(currentIndex);
+                if (!textAfter.isEmpty()) {
+                    printer.addTextAlign(Printer.ALIGN_CENTER);
+                    printer.addText(textAfter + "\n");
+                }
+            }
+
+            printer.addCut(Printer.CUT_FEED);
+
             printer.sendData(Printer.PARAM_DEFAULT);
-            printer.endTransaction();
-            printer.disconnect();
+
             callbackContext.success("Impression envoyée !");
+
+            printer.setReceiveEventListener(new ReceiveListener() {
+                @Override
+                public void onPtrReceive(Printer printer, int code, PrinterStatusInfo status, String printJobId) {
+                    Log.d("EpsonPrinterPlugin", "Callback reçu, job terminé");
+                    try {
+                        printer.endTransaction();
+                        printer.disconnect();
+                    } catch (Epos2Exception e) {
+                        callbackContext.error("Erreur impression : " + e.getErrorStatus());
+                    }
+                }
+            });
         } catch (Epos2Exception e) {
             callbackContext.error("Erreur impression : " + e.getErrorStatus());
+        }
+    }
+
+    private void isPrinterAvailable(CallbackContext callbackContext) {
+        try {
+            Printer printer = new Printer(Printer.TM_T88, Printer.MODEL_ANK, cordova.getActivity());
+            printer.connect("USB:", Printer.PARAM_DEFAULT);
+            printer.beginTransaction();
+
+            PrinterStatusInfo status = printer.getStatus();
+
+            boolean isOnline = status != null && status.getConnection() == Printer.TRUE && status.getOnline() == Printer.TRUE;
+
+            printer.endTransaction();
+            printer.disconnect();
+
+            if (isOnline) {
+                callbackContext.success("Imprimante disponible");
+            } else {
+                callbackContext.error("Imprimante non disponible");
+            }
+
+        } catch (Epos2Exception e) {
+            callbackContext.error("Erreur vérif dispo : " + e.getErrorStatus());
         }
     }
 }
--- www/epsonPrinter.js
+++ www/epsonPrinter.js
@@ -1,24 +1,37 @@
-(function(cordova) {
-    var EpsonPrinter = function () {};
+(function (cordova) {
+  var EpsonPrinter = function () {};
 
-    EpsonPrinter.prototype.printText = function (success, fail) {
-        return cordova.exec(
-            function (args) {
-                success(args);
-            },
-            function (args) {
-                fail(args);
-            },
-            'EpsonPrinterPlugin', // <--- nom exact de ta classe Java
-            'printText',          // <--- méthode dans ta classe Java
-            []
-        );
-    };
+  EpsonPrinter.prototype.printText = function (success, fail, options) {
+    return cordova.exec(
+      function (args) {
+        success(args);
+      },
+      function (args) {
+        fail(args);
+      },
+      "EpsonPrinterPlugin",
+      "printText",
+      [options]
+    );
+  };
 
-    window.EpsonPrinter = new EpsonPrinter();
+  EpsonPrinter.prototype.isPrinterAvailable = function (success, fail) {
+    return cordova.exec(
+      function (args) {
+        success(args);
+      },
+      function (args) {
+        fail(args);
+      },
+      "EpsonPrinterPlugin",
+      "isPrinterAvailable",
+      []
+    );
+  };
 
-    // rétrocompatibilité éventuelle
-    window.plugins = window.plugins || {};
-    window.plugins.EpsonPrinter = window.EpsonPrinter;
+  window.EpsonPrinter = new EpsonPrinter();
 
+  // rétrocompatibilité éventuelle
+  window.plugins = window.plugins || {};
+  window.plugins.EpsonPrinter = window.EpsonPrinter;
 })(window.PhoneGap || window.Cordova || window.cordova);
